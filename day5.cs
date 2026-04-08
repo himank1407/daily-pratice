fffc101aea475773da00c51d6e14c52f8b8095d8
himank <himank952@gmail.com>
2026-04-08 23:56:31 +0530

    day5

--- day5.cs
+++ day5.cs
@@ -2,13 +2,25 @@ string oddeven(int number)
 {
     if(number %2==0)
     {
-        return "it is even";
+        return "IT IS EVEN NUMBER";
     }
     else
     {
-        return  "it is odd";
+        return  "IT IS ODD NUMBER";
     }
+}
+string prime(int number)
+{
+    for(int i=2;i<number/2;i++)
+    {
+        if(number%i==0)
+        {
+            return " IT IS NOT A PRIME NUMBER";
+        }
+    }
+    return " IT IS A PRIME NUMBER";
 }
 Console.Write("ENTER A NUMBER :");
 int num=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(oddeven(num));
+Console.WriteLine(prime(num));
