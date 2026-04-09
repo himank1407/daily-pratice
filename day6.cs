01768ff44134a34f5ec84084325a3184c93410d2
himank <himank952@gmail.com>
2026-04-10 00:32:12 +0530

    day 6

--- day6.cs
+++ day6.cs
@@ -1,1 +1,8 @@
-Console.WriteLine("i dont know what i am trying to learn todya but . i think i will learn something");
+Console.WriteLine("i dont know what i am trying to learn today but  i think i will learn something");
+float h=12345.09876f;
+// Console.WriteLine(string.Format("{0;0}",h));
+// Console.WriteLine(string.Format("{0;0.00}",h));
+// Console.WriteLine(string.Format("{0;0.#}",h));
+Console.WriteLine($"{h:0}");
+Console.WriteLine($"{h:0.00}");
+Console.WriteLine($"{h:0.#}");
